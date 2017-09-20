29b4668ae63822deec997e0ebb80630fd0b5514d
Your Name <vineet.kothari@wwindia.com>
2017-09-20 14:56:46 +0530

    Added dashboard view and template

--- quiz/views.py
+++ quiz/views.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.models import User
+
+from quiz.models import Question, Answer, Tenant
+
+class DashboardView(View):
+
+	def get(self, request):
+		total_users = User.objects.all().count()
+		total_answers = Answer.objects.all().count()
+		total_questions = Question.objects.all().count()
+		tenants = Tenant.objects.all()
+		return render(request, "dashboard.html", {'total_users': total_users,
+												  'total_answers': total_answers,
+												  'total_questions': total_questions,
+												  'tenants': tenants})
